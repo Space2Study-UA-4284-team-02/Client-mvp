6ead21dc12e55ac6b2e8b87e5d09f9db237f0862
Anzhelika Voropai <sayurensa@gmail.com>
2026-03-22 12:43:28 +0200

    Disable unsafe eslint rules for test helpers

--- src/components/question-editor/QuestionEditor.test.helpers.tsx
+++ src/components/question-editor/QuestionEditor.test.helpers.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import type { ComponentProps } from 'react'
 import { render } from '@testing-library/react'
 import { vi } from 'vitest'
